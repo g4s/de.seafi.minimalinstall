aff561d649d95f9a66fd3a4b140737eb3180671a
Gregor Segner <gregor.segner@gmail.com>
2024-10-14 15:22:27 +0200

    adding loginprocess for user home init

--- templates/profile.d/00-initiate-user.sh
+++ templates/profile.d/00-initiate-user.sh
@@ -12,6 +12,18 @@ if [[ -e ${HOME}/.initiate ]]; then
   # installing ellipsis and source the installaton script
   source <(curl -s ellipsis.sh)
   export PATH=$PATH:${HOME}/.ellipsis/bin
+
+  if [[ $(command -v op) ]]; then
+    if [[ -z ${DISPLAY} ]]; then
+      user_email=$(zenity --entry --text "Enter 1password user emaul")
+      secret_key=$(zenity --password --text "Enter secret key")
+    else
+      read -p "Enter 1password user email: " user_email
+      read -s "Enter secret key: " secret_key
+    fi
+    op account add --address agilebit.1password.com --email ${user_mail} --secrete-key ${secret_key}
+  fi
+
   source <(curl -s ${REMOTE_SCRIPT})
 
   # finally remove the intiate-flag
